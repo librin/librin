6d8637cd843da80d2c40b875ec6cfcd19c143d70
pablogfb <pablobuntu@gmail.com>
2016-02-24 13:32:19 +0100

    Seeds file created and git ignore configured

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,7 +1,7 @@
 class Resource < ActiveRecord::Base
   has_many :documents
   has_and_belongs_to_many :tags
-has_attached_file :cover, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/introduccion_rails.png"
+has_attached_file :cover, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/missing.png"
 	validates_attachment :cover, content_type: { content_type: /\Aimage\/.*\Z/ } 
 	
 	
