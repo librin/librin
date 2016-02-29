5035f36a8f2597d89dfd01f9fc5382429fcef05d
irenemoreno <irene.morenocano@gmail.com>
2016-02-29 18:08:19 +0100

    avatar achieved

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ActiveRecord::Base
   has_many :comments
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  def avatar
+      email_address = self.email.downcase
+            # create the md5 hash
+      hash = Digest::MD5.hexdigest(email_address)
+            # compile URL which can be used in <img src="RIGHT_HERE"...
+      image_src = "http://www.gravatar.com/avatar/#{hash}"
+  end
 end
