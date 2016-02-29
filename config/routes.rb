00153017d52b26a727e81a298f6937a384e529a5
Pablo García <pablobuntu@gmail.com>
2016-02-29 09:22:28 +0100

    Devise updated

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
    
   get 'resource/download'
   
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "users/registrations" }
 root to: "resource#index"
  # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
