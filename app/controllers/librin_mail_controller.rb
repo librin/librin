8e6e5382315538a24301e8583efe6b9465d1ac14
pablogfb <pablobuntu@gmail.com>
2016-03-03 20:57:30 +0100

    suggestions through ajax

--- app/controllers/librin_mail_controller.rb
+++ app/controllers/librin_mail_controller.rb
@@ -4,13 +4,26 @@ class LibrinMailController < ApplicationController
     sender = current_user.userName
     resource = Resource.find(params[:id])
     user = User.where(:userName => params[:userName]).first
-    
+       
     if user == nil
-      redirect_to file_url + "?id=#{params[:id]}", notice: "El usuario no existe"
-      return
+      @alert="EL USUARIO NO EXISTE"
+      respond_to do |format|
+        format.js 
+        format.html{ 
+        redirect_to file_url + "?id=#{params[:id]}", notice: @alert
+        return
+      }
+      end
     else
     Librinmail.suggestion(sender, resource, user).deliver_now
+    @alert="RECOMENDACIÓN ENVIADA"
+          respond_to do |format|
+      format.js 
+      format.html{ 
+        redirect_to file_url + "?id=#{params[:id]}", notice: @alert
+        return
+      }
     end
-    redirect_to file_url + "?id=#{params[:id]}", notice: "Recomendación enviada"
   end
+ end
 end
